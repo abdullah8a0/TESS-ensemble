99019b9e48fd65bf2d384b7286d5dc84efba08f8
Muhammad Abdullah <abd880@mit.edu>
2021-11-01 12:40:33 -0400

    updating

--- lcobj.py
+++ lcobj.py
@@ -2,23 +2,36 @@ import numpy as np
 from astropy import stats as astat
 import matplotlib.pyplot as plt
 import pylab
-import scipy as sp
 from scipy import stats,signal
 import math
-from astropy.timeseries import LombScargle
+from pathlib import Path
+from sklearn.ensemble import IsolationForest
 
-base = "C:\\Users\\saba saleemi\\Desktop\\UROP\\TESS\\"                 # Change this to the folder where your transient_lcs is located
-ccd_path_raw = ["transient_lcs\\unzipped_ccd\\s","_transient_lc\\sector","\\cam","_ccd","\\lc_transient_pipeline\\"]
 
+def set_base(loc):
+    global base
+    base = loc
+
+
+#working_folder = "C:\\Users\\saba saleemi\\Desktop\\UROP\\TESS\\"
 
 def gen_path(sector,cam,ccd,col,row):
     # Generates path to specific file
     file_name = 'lc_'+str(col)+'.'+str(row)
+    cam,ccd,col,row = str(cam),str(ccd),str(col),str(row)
 
-    sector2 = str(sector) if sector > 9 else '0'+str(sector)
-    ccd_path = ccd_path_raw[0]+sector2+ccd_path_raw[1]+sector2+ccd_path_raw[2]+str(cam)+ccd_path_raw[3]+str(ccd)+ccd_path_raw[4]
+    #ccd_path_raw = ["s","_transient_lc/sector","/cam","_ccd","/lc_transient_pipeline/"]
+    ccd_path_raw = ["sector","/cam","_ccd","/lc_transient_pipeline/"]
+    sector2 = str(sector) if int(sector) > 9 else '0'+str(sector)
+    #ccd_path = ccd_path_raw[0]+sector2+ccd_path_raw[1]+sector2+ccd_path_raw[2]+str(cam)+ccd_path_raw[3]+str(ccd)+ccd_path_raw[4]
 
-    file_path = base + ccd_path + file_name
+    ccd_path = Path(ccd_path_raw[0]+sector2 + ccd_path_raw[1]+cam+ccd_path_raw[2]+ccd+ccd_path_raw[3])
+    try:
+        file_path = base / ccd_path / file_name
+    except NameError:
+        from run_classif import base
+        set_base(base)
+        file_path = base / ccd_path / file_name
     return file_path
 
 class LCOutOfBoundsError(Exception):
@@ -26,31 +39,43 @@ class LCOutOfBoundsError(Exception):
 class LCMissingDataError(Exception):
     pass
 
-class lc_obj(object):
+class LC(object):
     def __init__(self, sector, cam, ccd, col, row):
+        mask = {
+            35: (2268.50, 2272.045),
+        }
+        
+
         try:
             assert 44 <= int(col) <= 2097
             assert 0 <= int(row) <= 2047 
         except AssertionError:
-            raise LCOutOfBoundsError
+            raise LCOutOfBoundsError(f'Out of bouds: {col},{row}')
 
         self.path = gen_path(sector,cam,ccd,col,row)
-        self.sector = sector
-        self.cam = cam
-        self.ccd = ccd
+        self.sector = int(sector)
+        self.cam = int(cam)
+        self.ccd = int(ccd)
         self.coords = (int(col),int(row))
         lc_data = np.genfromtxt(self.path)
 
         try:
             assert len(lc_data.shape) == 2
         except AssertionError:
-            raise LCMissingDataError
-
-
-        self.flux_unclipped = -lc_data[:, 1]
-        self.time_unclipped = lc_data[:, 0]
-        self.error_unclipped = lc_data[:, 2]
-        self.bg_unclipped = -lc_data[:, 6]
+            raise LCMissingDataError("The flux file is almost empty")
+        if not np.all(np.isnan(lc_data[0,:])):
+            self.flux_unclipped = -lc_data[:, 1]
+            self.time_unclipped = lc_data[:, 0]
+            self.error_unclipped = lc_data[:, 2]
+            self.bg_unclipped = -lc_data[:, 6]
+            self.iscleaned = False
+        else:
+            new_lc = lc_data[1:,:].astype('float64')
+            self.flux_unclipped = new_lc[:, 2]
+            self.time_unclipped = new_lc[:, 1]
+            self.error_unclipped = new_lc[:, 3]
+            self.bg_unclipped = new_lc[:, 4]
+            self.iscleaned = True
         
         bg_clip = astat.sigma_clip(self.bg_unclipped,sigma=3)
 
@@ -59,49 +84,108 @@ class lc_obj(object):
         self.error = np.array([self.error_unclipped[i] for i in np.ma.nonzero(bg_clip)[0]])
         self.bg = np.array([self.bg_unclipped[i] for i in np.ma.nonzero(bg_clip)[0]])
 
+        self.error = np.nan_to_num(self.error,nan=100)
+
+
+        arr_un, unique = np.unique(self.time,return_index=True)
+        self.flux = self.flux[unique] 
+        self.time  =self.time[unique]        
+        self.error =self.error[unique]
+        self.bg =   self.bg[unique] 
         if len(self.flux) < 10:
-            raise TypeError
+            raise LCMissingDataError("The flux file has less than 10 entries")
+        if sector in mask.keys():
+            masking_arr = np.logical_or(self.time < mask[sector][0],self.time > mask[sector][1])
+
+            self.flux   = self.flux[masking_arr]
+            self.time   = self.time[masking_arr]
+            self.error  = self.error[masking_arr]
+            self.bg     = self.bg[masking_arr]   
+
 
         self.N = len(self.flux)
         self.mean = np.mean(self.flux)
         self.std = np.std(self.flux)
 
+        try:
+            assert self.N > 60
+        except AssertionError:
+            raise LCMissingDataError("The flux file has less than 60 entries")
 
         # Smoothing using SavGol Filter
-        
-        self.smooth_flux = signal.savgol_filter(self.flux, min((1|int(0.05*self.N),61)), 3)
+        try:
 
+            self.smooth_flux = signal.savgol_filter(self.flux,min((1|int(0.05*self.N),61)), 3)
+        except:
+            raise LCMissingDataError
         self.linreg = stats.linregress(self.time,self.flux)[0:3]  #(slope,c,r)
 
         self.is_padded = False
         self.is_FFT = False         # Flags whether the instance has these attributes
         self.is_percentiles = False  
 
-        self.normed_flux = (self.flux - self.mean)/np.max(np.abs(self.flux))
+        self.normed_flux = (self.flux - self.mean)/np.max(np.abs(self.flux-self.mean))
+        self.normed_time = self.time/np.max(self.time)
+        self.normed_smooth_flux = (self.smooth_flux - self.mean)/np.max(np.abs(self.smooth_flux-self.mean))
 
-    def plot(self, flux = None, time = None, show_bg = True, show_err = False):
+    def plot(self, flux = None, time = None, show_bg = True, show_err = False,show_smooth=False,scatter=[]):
         flux = self.flux if flux is None else flux
         time = self.time if time is None else time
 
         #fig = pylab.gcf()
         #fig.canvas.manager.set_window_title('Figure_'+str(self.coords[0])+'_'+str(self.coords[1]))
         plt.xlabel("Time (Days)")
-        plt.ylabel("True Flux (/10^6)")
+        plt.ylabel("Raw Flux")
         plt.scatter(time,flux,s=0.5)
         if show_bg:
             plt.scatter(time,self.bg,s=0.1)
         if show_err:
             plt.errorbar(time, flux, yerr=self.error, fmt ='o' )
+        if show_smooth:
+            plt.scatter(time,self.smooth_flux,s=0.1)
+        for data in scatter:
+            try:
+                time_s,flux_s = data
+                plt.scatter(time_s,flux_s,s=0.1)
+            except:
+                plt.scatter(time,data,s=0.1)
         plt.show()
+        return self
 
+    def remove_outliers(self):
+        outlier_detector = IsolationForest(n_estimators=200,max_samples=1000,random_state=np.random.RandomState(314),contamination=0.01)  # dynamic percentage?
+        forest = outlier_detector.fit_predict(list(zip(b:=self.normed_flux,a:=np.arange(0,1,1/len(self.normed_flux)))))
+        #forest = outlier_detector.fit_predict(self.normed_flux.reshape(-1,1))
+        inliers = np.ma.nonzero(forest==1)[0]
+        self.normed_flux = self.normed_flux[inliers]
+        self.normed_time = self.normed_time[inliers]
+        self.flux        = self.flux[inliers]
+        self.time        = self.time[inliers]
+        self.error       = self.error[inliers]
+        self.bg          = self.bg[inliers]
+        self.N           = inliers.size
+        self.mean        = np.mean(self.flux)
+        self.std         = np.std(self.flux)
+        self.median      = np.median(self.flux)
+        try: 
+            self.smooth_flux = signal.savgol_filter(self.flux,min((1|int(0.05*self.N),61)), 3)
+        except:
+            raise LCMissingDataError
+
+        if self.N < 60:
+            raise LCMissingDataError
+
+        return self
     def make_percentiles(self):
         self.percentiles = {i:np.percentile(self.flux,i) for i in range(0,101,1)}
         self.is_percentiles = True
 
-    def flat(self, smooth = False):
-        flux = self.smooth_flux if smooth else self.flux
-        m, c, _ = self.linreg
-        return flux - (m*self.time +c)
+    def flatten(self, smooth = None):
+        flux = self.smooth_flux if smooth is None else self.flux
+
+        fit = signal.savgol_filter(self.flux, 201 , 1)
+        self.flat =  flux - fit
+        return self
 
     def pad_flux(self):
         perday = 48 if self.sector <= 26 else 144
@@ -154,24 +238,6 @@ class lc_obj(object):
         plt.scatter(freq,pow,s=5)
         plt.show()        
 
-    '''
-    def make_periodogram(self):
-        self.LS_object = LombScargle(self.time, self.normed_flux, self.error)
-        freq, pow = self.LS_object.autopower()    # Check with supervisor to set appropriiate minimum and max frequencies
-        self.periodogram = (freq,pow)
-        self.computed_freq = abs(freq[np.argmax(pow)])
-        self.computed_freq_power = np.max(pow)
-        self.is_LombScargle = True
-    
-        T = 1/self.computed_freq
-
-        phase = self.time - self.time[0]
-        for i in range(len(phase)):
-            while phase[i] > T:
-                phase[i] -= T
-
-        self.phase_space = phase        # Folded time space
-    '''
 
 
     def plot_phase(self):
@@ -189,3 +255,60 @@ class lc_obj(object):
         plt.scatter(np.arange(len(self.smooth_flux)), self.smooth_flux, s=0.5)
         plt.show()
 
+class TagNotFound(Exception):
+    pass
+class TagFinder:
+    def __init__(self,tags_original):
+
+        tags = np.copy(tags_original)
+
+        tags = tags.tolist()
+        tags = sorted(enumerate(tags), key =lambda x:x[1])
+        map = {i:tags[i][0] for i in range(len(tags))}
+        tags = np.array([x[1] for x in tags])                 # reorder feat with tags
+        self.tags = tags
+        self.map = map
+    
+    def find(self,tag):
+        return self.map[self.bin_search(np.array(tag))]
+
+
+
+    def gt(self,x,y):
+        return self.lt(y,x)
+
+    def bin_search(self,tag) -> int:
+        tags = self.tags
+        upper = tags.shape[0]-1
+        lower = 0
+        while lower <= upper:
+            mid = (upper + lower)//2
+            if self.lt(tags[mid], tag):
+                lower = mid + 1
+            elif self.gt(tags[mid] , tag):
+                upper = mid - 1
+            else:
+                return mid
+        raise TagNotFound(f"Tag is not in the data : {tuple(tag)}")
+
+    def lt(self,x,y):
+        if (x==y).all():
+            return False
+        idx = np.where((x!=y))[0][0]
+        #print(x[idx])
+        return x[idx]< y[idx]
+
+
+
+def get_coords_from_path(file_path):
+    # returns coords from any file path
+    file_path = str(file_path)
+    i = file_path.rfind('lc_')
+    d = file_path.rfind('.')
+
+    x = file_path[i+3:d]
+    y = file_path[d+1:]
+    return (x,y)
+if __name__ == '__main__':
+    pass
+    #set_base("C:\\Users\\saba saleemi\\Desktop\\UROP\\TESS\\transient_lcs\\unzipped_ccd\\")                 # Change this to the folder where your sxx_transient_lcs is located
--- plot_lc.py
+++ plot_lc.py
@@ -1,94 +1,125 @@
+from matplotlib import pyplot as plt
+from numpy.core.shape_base import block
+from cleanup_anomaly import isdirty
+from TOI_gen import TOI_list
+from cluster_anomaly import hdbscan_cluster
 import os
+import math
+from scipy import signal, stats
+from sklearn.mixture import GaussianMixture
+from numpy.random import shuffle
 import lcobj
-import matplotlib.pyplot as plt
 import numpy as np
-from scipy.signal import find_peaks
-def get_coords_from_path(file_path):
-    # returns coords from any file path
-    i = file_path.rfind('lc_')
-    d = file_path.rfind('.')
+from lcobj import LC, get_coords_from_path
+from run_classif import base
+from pathlib import Path
+import plotly.express as px
+import time
 
-    x = file_path[i+3:d]
-    y = file_path[d+1:]
-    return (x,y)
-
-"""
-def clip(table, base, sigma):
-    # removes entries from 'table' where the 'base'
-    # has variation more than 'sigma' 
-    base_clip = astat.sigma_clip(base,sigma=sigma)
-    return np.array([table[i] for i in np.ma.nonzero(base_clip)[0]])
+#base = "C:\\Users\\saba saleemi\\Desktop\\UROP\\TESS\\transient_lcs\\unzipped_ccd\\" # Forced value of base
 
 
 
-    #### DEADZONE CHECK
+lcobj.set_base(base)
 
-    coords = get_coords_from_path(file_path)    # column,rows
-    assert 43 < int(coords[0]) < 2091                                                # check for off by one
-    assert int(coords[1]) < 2057
-    print(f'CCD pixel: {coords}')
 
-    ####
-"""
+def plotter():
+    choice = input("plot all, plot one, plot result file, plot transients candidates: ")
 
-if __name__ == '__main__':
-    choice = input("plot all? y/n: ")
+    sector, cam, ccd = 32,4, 4
 
-    sector, cam, ccd = 6, 1, 1
-
-    path = lcobj.gen_path(sector,cam,ccd,0,0)[:-6]
-    if choice == 'y':
+    path = Path(str(lcobj.gen_path(sector,cam,ccd,0,0))[:-6])
+    if choice == '0':
         user_in = None 
         with os.scandir(path) as entries:
             for i,entry in enumerate(entries):
                 if not entry.name.startswith('.') and entry.is_file():
                     file_name = str(entry.name)
                     col, row = get_coords_from_path(file_name)
-                    print(col,row)
-                    try:
-                        lc = lcobj.lc_obj(sector, cam, ccd, col, row)
-                    except:
-                        continue
-                    lc.plot()
-                    #lc.smooth_plot()
-                    #lc.plot(flux=lc.flat(smooth=True))
-                    #peaks = find_peaks(lc.smooth_flux, prominence=5, distance=10, width=10)[0]
-                    #plt.scatter(lc.time[:len(lc.smooth_flux)],lc.smooth_flux, s= 1)
-
-                    #plt.scatter(lc.time[peaks], lc.smooth_flux[peaks])
-                    #plt.show()
-                    if user_in != '0':
-                        user_in = input("Press Enter to continue, type 0 to plot all: ")
-                        if user_in == '1':
-                            break
-    else:
+                    print((sector,cam,ccd, col,row))
+                    tag = (sector,cam,ccd,col,row)
+                    lc = lcobj.LC(*tag).plot() 
+    elif choice == '1':
         name = None
         while True:
-            name = input('what is the file name? (0 to exit): ')
+            name = input('what is the file? (0 to exit): ')
             if name == '0':
                 break
-            col, row = get_coords_from_path(name)
-            lc = lcobj.lc_obj(sector,cam,ccd,col,row)
-            #lc.make_periodogram()
-            #print(lc.computed_freq_power)
-            #plt.scatter(*lc.periodogram, s= 0.5)
-            #plt.show()
-            '''
-            ind = np.argpartition(lc.periodogram[1], -10)[-10:]
-            top_10_freq = lc.periodogram[0][ind]
-            generic = []
-            for i in range(10):
-                if 95.8< top_10_freq[i] < 96.2:
-                    generic.append(i)
-                if 47.8 < top_10_freq[i] < 48.2:
-                    generic.append(i)
-            result = np.delete(top_10_freq,generic)
-
-            print("Top ten: ", result)
-
-            lc.plot_phase()
-            '''
-            #p_coeff = np.sum(lc.smooth_flux) 
-            #print(p_coeff)
-            lc.smooth_plot()            
+            sec, cam,ccd,col, row = name.split()
+            try:
+                lc = lcobj.LC(sec,cam,ccd,col,row)
+            except OSError:
+                continue
+            print(f'is_cleaned: {lc.iscleaned}')
             lc.plot()
+
+    elif choice == '2':
+        sectors = input('file name: ').split()
+        file_name = '_'.join(sectors)
+        file_path = Path(f'Results/{file_name}.txt')
+        feat_tag,feat_data = next(lcobj.get_sector_data(sector,'s',verbose=False))
+
+        raw_data = np.genfromtxt(file_path,delimiter = ',')
+        tags = raw_data[:,:4].astype('int32')
+        rand_tags = np.copy(tags)
+        shuffle(rand_tags)
+        for i,tag in enumerate(rand_tags):
+
+            print((i,int(sectors[0]), *tag))
+            cam,ccd,col,row = tag
+            
+            found,i = False,0
+            while not found:
+                try:
+                    lc = lcobj.LC(int(sectors[i]),cam,ccd,col,row)
+                    sec = sectors[i]
+                    found = True
+                except OSError:
+                    i+=1
+            tag = np.array([cam,ccd,col,row]).astype('int32')
+            lc.remove_outliers()
+                     
+            peaks = signal.find_peaks(lc.smooth_flux,prominence=6,distance=50)[0]
+            lc.plot(show_smooth=True,show_bg=False)
+            plt.show()
+    elif choice == '3':
+        sectors = input('file name: ')
+        file_name = sectors + '_transients'
+        file_path = f'{file_name}.txt'
+        feat_tag,feat_data = next(lcobj.get_sector_data(sector,'s',verbose=False))
+        with open(file_path) as file:
+            
+            for i,tag in enumerate(file):
+                print(i)
+                sec,cam,ccd,col,row = tag.split()
+                lc = lcobj.LC(int(sec),cam,ccd,col,row)
+                print(tag)
+                tag = np.array([cam,ccd,col,row]).astype('int32')
+                print(feat_data[np.ma.nonzero([ (x==tag).all() for x in feat_tag]),-4])
+                print(feat_data[np.ma.nonzero([ (x==tag).all() for x in feat_tag]),9])
+                lc.plot()
+    elif choice == '4':
+
+        file_name = input('file name: ')
+        file_path = f'{file_name}.txt'
+        feat_tag,feat_data = next(lcobj.get_sector_data(sector,'s',verbose=False))
+        with open(file_path) as file:
+            
+            for i,tag in enumerate(file):
+                print(i)
+                try:
+                    sec,cam,ccd,col,row = tag.split(',')
+                except ValueError:
+
+                    cam,ccd,col,row = tag.split(',')
+                    sec = sector
+                print(repr(row))
+                lc = lcobj.LC(int(sec),cam,ccd,col,row[:-1])
+                print(tag)
+                tag = np.array([cam,ccd,col,row]).astype('int32')
+                print(feat_data[np.ma.nonzero([ (x==tag).all() for x in feat_tag]),-4])
+                print(feat_data[np.ma.nonzero([ (x==tag).all() for x in feat_tag]),11])
+                lc.plot()
+
+if __name__ == '__main__':
+    plotter()
